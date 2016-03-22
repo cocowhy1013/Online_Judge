a6e68d625b745b73387a6ce991f06794493b51fc
Coco_Wang <cocowhy1013@gmail.com>
2016-03-22 18:57:31 +0800

    No292: NimGame from LeetCode solved

---
+++ src/No292_NimGame.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Coco on 2016/3/22.
+ */
+public class No292_NimGame {
+ //   public class Solution {
+        public boolean canWinNim(int n) {
+            return !(n % 4 == 0);
+        }
+   // }
+}
